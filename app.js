76aa0701e9d58dfb42d9f29b8b43967483159964
Siddharth Golla <siddharth.golla87@gmail.com>
2022-08-15 14:39:32 +0530

    Attached sections to buttons

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function PageTransitions(){
+
+    // Active button class activation on click
     for(let i = 0; i < sectBtn.length; i++){
         sectBtn[i].addEventListener('click', function(){
             let currentBtn = document.querySelectorAll('.active-btn');
@@ -11,6 +13,26 @@ function PageTransitions(){
             this.className += ' active-btn'
         })
     }
+
+    // Active section activation on click
+    allSections.addEventListener('click', (e) => {
+        const id = e.target.dataset.id;
+        if (id){
+            // Remove the selected button from other buttons
+            sectBtns.forEach((btn) => {
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            // Hide other sections
+            sections.forEach((section) => {
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 PageTransitions();
